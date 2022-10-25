5c31a62c49aa789b582a405faa5582177eb8f9cb
Rafael Ribeiro <rafael.ribeirowt@gmail.com>
2022-10-25 16:01:15 -0300

    Payment, one-to-one association

--- src/main/java/com/raeltecnologia/course/config/TestConfig.java
+++ src/main/java/com/raeltecnologia/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.raeltecnologia.course.entities.Category;
 import com.raeltecnologia.course.entities.Order;
 import com.raeltecnologia.course.entities.OrderItem;
+import com.raeltecnologia.course.entities.Payment;
 import com.raeltecnologia.course.entities.Product;
 import com.raeltecnologia.course.entities.User;
 import com.raeltecnologia.course.entities.enums.OrderStatus;
@@ -46,7 +47,7 @@ public class TestConfig implements CommandLineRunner {
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.DELIVERED, u1); 
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1); 
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.DELIVERED, u2); 
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, u1);
 		
@@ -82,5 +83,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment (null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.saveAll(Arrays.asList(o1));
+		
+		
 	}
 }
--- src/main/java/com/raeltecnologia/course/entities/OrderItem.java
+++ src/main/java/com/raeltecnologia/course/entities/OrderItem.java
@@ -66,6 +66,7 @@ public class OrderItem implements Serializable{
 		this.price = price;
 	}
 	
+	
 	public Product getProduct() {
 		return id.getProduct();
 	}
