5d1e97c37734fb168851d8845085f1a2a0d1bc94
Rafael Ribeiro <rafael.ribeirowt@gmail.com>
2022-10-25 19:27:52 -0300

    Exception handling - update

--- src/main/java/com/raeltecnologia/course/services/UserService.java
+++ src/main/java/com/raeltecnologia/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.raeltecnologia.course.repositories.UserRepository;
 import com.raeltecnologia.course.resouces.exceptions.DatabaseException;
 import com.raeltecnologia.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -35,27 +37,31 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		
+
 		try {
-		repository.deleteById(id);
-		}catch(EmptyResultDataAccessException e ) {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		}catch(DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
-	
+
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
-		
+
 	}
 }
