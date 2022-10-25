f990ff476531872395ab589f4d669a36a0b53e1d
Rafael Ribeiro <rafael.ribeirowt@gmail.com>
2022-10-25 00:21:07 -0300

    Order, Instant, ISO 8601 Part 2

---
+++ src/main/java/com/raeltecnologia/course/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.raeltecnologia.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.raeltecnologia.course.entities.Order;
+
+
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
