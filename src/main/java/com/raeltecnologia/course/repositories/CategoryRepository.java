b1147c2b94578499a6a87c46a8ac8808ce112494
Rafael Ribeiro <rafael.ribeirowt@gmail.com>
2022-10-25 09:30:03 -0300

    Category

---
+++ src/main/java/com/raeltecnologia/course/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.raeltecnologia.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.raeltecnologia.course.entities.Category;
+
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
