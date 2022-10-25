288634f0f84a6a39de28d855548ae9a19030afdf
Rafael Ribeiro <rafael.ribeirowt@gmail.com>
2022-10-25 19:20:24 -0300

    Exception handling - delete

--- src/main/java/com/raeltecnologia/course/resouces/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/raeltecnologia/course/resouces/exceptions/ResourceExceptionHandler.java
@@ -23,5 +23,16 @@ public class ResourceExceptionHandler {
 		
 	}
 	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request ){
+		String error = "Database Error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+		
+	}
+	
+	
+	
 	
 }
